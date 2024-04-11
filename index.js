bb09beb38ef0c9bb17f1c2d6872a5c5b82596aff
nullisdefined <jaeuu.dev@gmail.com>
2024-04-11 16:24:06 +0900

    로컬에서 updata index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 console.log("index.js");
 
 console.log("second commit");
+
+console.log("push 충돌 상황");
+=======
+console.log("index.js");
+
+console.log("second commit");
+
+// 깃허브 웹에서 원격 저장소 수정..
+>>>>>>> 03dc60da1aeda062fe68d818a75703938e38ec48
